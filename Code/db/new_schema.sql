3e0edbc9fc9b7efecf469a430031f992b45373a6
sumanra <sumanra@users.noreply.github.com>
2019-08-06 13:38:35 -0400

    adding the view

--- Code/db/new_schema.sql
+++ Code/db/new_schema.sql
@@ -1,25 +1,6 @@
 CREATE DATABASE stock_data;
 USE stock_data;
-
-SELECT * FROM stock_data.tech_data;
-
-CREATE TABLE combine_lstm (
-Dates date,
-Close float,
-Predictions float,
-Symbols text
-);
-
-drop table combine_lstm
-
-
-select * from cfg_data_lstm;
-
-CREATE VIEW test_union_view2 AS
-SELECT * FROM axp_data_lstm
-UNION
-SELECT * FROM cfg_data_lstm;
-
+-- Creating a view by combining all the LSTM tables
 CREATE VIEW combine AS
 select * from axp_data_lstm
 union
